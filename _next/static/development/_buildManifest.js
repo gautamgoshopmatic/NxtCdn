7c5f5c7bcf842fc288b0118e933848b827d916be
gautamgoshopmatic <gautam.fonseca@goshopmatic.com>
2021-08-30 14:53:36 +0530

    initial commit

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[{source:"\u002Frobots.txt",destination:"\u002Fapi\u002Frobots"},{source:"\u002Fmanifest.json",destination:"\u002Fapi\u002Fmanifest"}],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
